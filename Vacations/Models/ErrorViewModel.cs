09d1c6a3749c8148e930d0f8c0239cf62496f471
JuliaTitto87 <133301250+JuliaTitto87@users.noreply.github.com>
2024-04-10 09:42:58 +0300

    New project was created

---
+++ Vacations/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vacations.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
